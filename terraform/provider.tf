d543e1cca25ff760c067e89e01675361ce553129
Renata Psevodska <renatalit@yahoo.com>
2025-04-04 10:12:03 -0500

    terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,8 +3,13 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.40.0"
-      shared_credentials_files = ["/home/vagrant/.aws/credentials"]
     }
   }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  # Change this region to match your default region!!!!!!!!!!!!!
+  region = "us-east-2"
+  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+}
